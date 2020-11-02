6650b4eb4b44080e49f2d53a9654ca123b762304
Andres Ruiz <aruiz@grupooctano.com.mx>
2020-11-02 09:14:12 -0600

    Clase 20

--- diablo3/src/views/Profile/MainBlock/Index.vue
+++ diablo3/src/views/Profile/MainBlock/Index.vue
@@ -5,19 +5,21 @@
       <HeroesList v-if="hasHeroesList" :heroes="heroesList"/>
       <ProgressList :acts="profileData.progression"/>
     </div>
+    <!-- Right Bar-->
     <div class="grid-item item-right">
-      <h1>Derecha</h1>
+      <PlayerStats :stats="statsData"/>
     </div>
   </div>
 </template>
-
 <script>
 import TopHeroes from './TopHeroes/Index'
 import HeroesList from './HeroesList/Index'
 import ProgressList from './ProgressList/Index'
+import PlayerStats from './PlayerStats/Index'
+
 export default {
   name: 'MainBlock',
-  components: { ProgressList, TopHeroes, HeroesList },
+  components: { PlayerStats, ProgressList, TopHeroes, HeroesList },
   props: {
     profileData: {
       type: Object,
@@ -36,6 +38,10 @@ export default {
     },
     heroesList () {
       return this.profileData.heroes.slice(3, this.profileData.heroes.length)
+    },
+    statsData () {
+      const { paragonLevel, kills, timePlayed } = this.profileData
+      return { paragonLevel, kills, timePlayed }
     }
   }
 }
